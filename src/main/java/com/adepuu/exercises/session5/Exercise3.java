7f5323a43f23f6188619a135051a8c5a003e2d4c
jeffmartinedvan <jeffmartinedvan@gmail.com>
2024-03-25 15:10:46 +0700

    feat: exercise 3

--- src/main/java/com/adepuu/exercises/session5/Exercise3.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise3.java
@@ -1,5 +1,9 @@
 package com.adepuu.exercises.session5;
 
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashSet;
+
 public class Exercise3 {
     /**
      * Write a Java Program to Check if Array Contain Duplicates
@@ -17,6 +21,22 @@ public class Exercise3 {
      * Output: true
      */
     public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 1};
+        int[] array2 = {1, 2, 3, 4};
+        int[] array3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        System.out.println(hasDuplicates(array1));
+        System.out.println(hasDuplicates(array2));
+        System.out.println(hasDuplicates(array3));
+    }
 
+    public static boolean hasDuplicates(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
     }
 }
