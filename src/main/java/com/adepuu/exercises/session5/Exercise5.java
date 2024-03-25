e653cf65abd85a2f5aa5fa76037a6e9ff4bb3c3a
jeffmartinedvan <jeffmartinedvan@gmail.com>
2024-03-25 23:58:14 +0700

    feat: exercise 5 using bubble sort method

--- src/main/java/com/adepuu/exercises/session5/Exercise5.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise5.java
@@ -12,7 +12,46 @@ public class Exercise5 {
      * Input: [ 8, 7, 5, 2], direction = “desc”
      * Output: [ 8,7, 5, 2 ]
      */
-    public static void main(String[] args) {
+    //Method to sort arrays
+    public static void ascSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++){
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 
+    public static void descSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++){
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr = {8, 7, 5, 2};
+
+        ascSort(arr);
+        System.out.print("Ascending Order: ");
+        printArray(arr);
+
+        descSort(arr);
+        System.out.print("Descending Order: ");
+        printArray(arr);
     }
 }
