46fc347f72e8e96548ad172e132902b2503ba5e4
jeffmartinedvan <jeffmartinedvan@gmail.com>
2024-03-25 13:28:28 +0700

    feat: exercise 1

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,20 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise1 {
+    public static void main(String[] args) {
+
+        int[] array1 = { 7, 2, 5, 1, 4 };
+        System.out.println("The highest number in the array is: " + findHighestInt(array1));
+    }
+
+    //Method to find the highest int in the array
+    public static int findHighestInt(int[] arrayNumbers) {
+        int maxInt = arrayNumbers[0]; //set the first index to maxInt
+        for (int number : arrayNumbers) {
+            if (number > maxInt) {
+                maxInt = number;
+            }
+        }
+        return maxInt;
+    }
 }
