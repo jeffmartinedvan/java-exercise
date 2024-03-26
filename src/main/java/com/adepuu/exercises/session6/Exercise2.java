44d3e96b3f64bdc0f1593a6b3b3011e3b9681345
jeffmartinedvan <jeffmartinedvan@gmail.com>
2024-03-26 14:15:43 +0700

    feat: exercise 2

--- src/main/java/com/adepuu/exercises/session6/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise2.java
@@ -1,27 +1,38 @@
-//package com.adepuu.exercises.session6;
-//
-//import java.io.BufferedReader;
-//import java.io.FileReader;
-//
-//public class Exercise2 {
-//    /**
-//     * Complete the file reader program below
-//     * and handle all edge cases using try-catch-finally based on the logic given
-//     */
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Enter the file name: ");
-//        String fileName = scanner.nextLine();
-//        readFileContents(fileName);
-//        scanner.close();
-//    }
-//
-//    static void readFileContents(String fileName) {
-//        BufferedReader reader = null;
-//        reader = new BufferedReader(new FileReader(fileName));
-//        String line;
-//        while ((line = reader.readLine()) != null) {
-//            System.out.println(line);
-//        }
-//    }
-//}
+package com.adepuu.exercises.session6;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Exercise2 {
+    /**
+     * Complete the file reader program below
+     * and handle all edge cases using try-catch-finally based on the logic given
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the file name: ");
+        String fileName = scanner.nextLine();
+        readFileContents(fileName);
+        scanner.close();
+    }
+
+    static void readFileContents(String fileName) {
+        try {
+            BufferedReader reader = null;
+            reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch(FileNotFoundException e) {
+            System.out.println("File is not found");
+        } catch(IOException e) {
+            System.out.println("Something went wrong");
+        } finally {
+            System.out.println("Code is completed");
+        }
+    }
+}
